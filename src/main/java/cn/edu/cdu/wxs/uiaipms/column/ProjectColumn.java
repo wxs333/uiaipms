3e9b5459b9f72dfb449830cdafa5620fffc0775d
WXS333 <1355177952@qq.com>
2020-04-04 21:17:44 +0800

    项目重申报完成

---
+++ src/main/java/cn/edu/cdu/wxs/uiaipms/column/ProjectColumn.java
@@ -0,0 +1,27 @@
+package cn.edu.cdu.wxs.uiaipms.column;
+
+/**
+ * 项目表 字段映射
+ *
+ * @author WXS
+ * @date 2020/4/4
+ */
+public class ProjectColumn {
+    public static final String PRO_ID = "pro_id";
+
+    public static final String PRO_NAME = "pro_name";
+
+    public static final String STU_ID = "stu_id";
+
+    public static final String WORD_NAME = "word_name";
+
+    public static final String PRO_LOCATION = "pro_location";
+
+    public static final String PRO_DESC = "pro_desc";
+
+    public static final String UPDATE_TIME = "update_time";
+
+    public static final String CREATE_TIME = "create_time";
+
+    public static final String LOGIC_DELETE_FLAG = "logic_delete_flag";
+}
---
+++ src/main/resources/static/js/student/pro_afresh.js
@@ -0,0 +1,59 @@
+layui.use(['upload', 'form', 'layer'], function () {
+    var $ = layui.jquery;
+    var _upload = layui.upload;
+    var _form = layui.form;
+    var _layer = layui.layer;
+    // 监听表单提交
+    _form.on('submit', function (data) {
+        // 数据提交导后台
+        add($, _layer, data.field);
+        return false;
+    });
+
+    wordUpload(_upload, $, _layer);
+});
+
+/**
+ * Word文档上传
+ */
+function wordUpload(_upload, $, _layer) {
+    _upload.render({
+        elem: '#word',
+        url: '/api/pro/uploadWord',
+        headers: {'method': 'post'},
+        accept: 'file',
+        acceptMime: 'file/doc, file/docx',
+        exts: 'doc|docx',
+        multiple: true,
+        choose: function (obj) {
+            obj.preview(function (index, file, result) {
+                $("#wordName").text("已选择：" + file.name);
+            });
+        },
+        done: function (res) {
+            var icon = res.code === "200" ? 1 : 2;
+            _layer.msg(res.msg, {time: 1500, icon: icon}, function () {
+                $("#proLocation").val(res.proLocation);
+                $("#hide-word-name").val(res.wordName);
+                $("#submit").removeClass("layui-btn-disabled");
+            });
+        }
+    })
+}
+
+/**
+ * 项目申报
+ */
+function add($, _layer, data) {
+    $.post(
+        '/api/pro/afresh',
+        data,
+        function (res) {
+            var icon = res.code === 'success' ? 1 : 2;
+            _layer.msg(res.message, {time: 1500, icon: icon}, function () {
+                var index = parent.layer.getFrameIndex(window.name);
+                parent.layer.close(index);
+            });
+        }
+    )
+}
